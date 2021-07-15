810d49bc839992e3ff8c63b5871728847c4da6e6
Julian Gernun <julian.gernun@adevinta.com>
2021-07-15 09:08:39 +0200

    chore(sui-ssr): dont add package-lock on clean install

--- packages/sui-ssr/bin/sui-ssr-release.js
+++ packages/sui-ssr/bin/sui-ssr-release.js
@@ -90,12 +90,14 @@ const getCommitToTag = async () => {
 
     await execute('npm version minor --no-git-tag-version')
     const nextVersion = require(path.join(cwd, 'package.json')).version
-    await execute(
-      `git add ${path.join(cwd, 'package.json')} ${path.join(
-        cwd,
-        'package-lock.json'
-      )}`
-    )
+    if (!clean) {
+      await execute(
+        `git add ${path.join(cwd, 'package.json')} ${path.join(
+          cwd,
+          'package-lock.json'
+        )}`
+      )
+    }
 
     await execute(`git commit -m "release(META): ${nextVersion}"`)
     await execute(`git tag -a "v${nextVersion}" -m "v${nextVersion}"`)
